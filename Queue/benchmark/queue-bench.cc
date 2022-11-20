18ca9f89c719cf6b721617c0ec540fa668f638dd
Aleksey Otrashchenko <aleksey.ot@gmail.com>
2022-11-20 21:40:06 +0300

    [Queue]: added benchmark which stack-queue loses

--- Queue/benchmark/queue-bench.cc
+++ Queue/benchmark/queue-bench.cc
@@ -1,9 +1,13 @@
 #include <benchmark/benchmark.h>
 #include "queue.hh"
+#include <vector>
 
 constexpr size_t RangeMultiplier = 2;
-constexpr size_t RangeStart = 1 << 15;
+constexpr size_t RangeStart = 1 << 17;
 constexpr size_t RangeFinish = 1 << 20;
+
+constexpr size_t HeavyRangeStart = 1 << 12;
+constexpr size_t HeavyRangeFinish = 1 << 15;
 
 template <typename T>
 struct PairQueueListType
@@ -51,12 +55,57 @@ static void QueuePushing(benchmark::State& state)
     }
 }
 
+template <class PairContType> 
+static void QueueHeavyPushing(benchmark::State& state)
+{
+    using TestType = typename PairContType::Type;
+    using QueueType = typename PairContType::QueueType;
+
+    size_t push_vec_size = 1e3;
+    size_t assign_vec_size = 1e3;
+
+    std::vector<double> to_push(push_vec_size);
+    std::vector<double> to_assign(assign_vec_size);
+
+    for (auto _ : state)
+    {
+        QueueType queue;
+        for (size_t i = 0; i < state.range(); i++)
+        {
+            queue.push(to_push);
+            auto&& front = queue.front();
+            front = to_assign;
+            auto&& back = queue.back();
+            back = to_assign;
+        }
+
+        for (size_t i = 0; i < state.range() - 1; i++)
+        {
+            queue.pop();
+            auto&& front = queue.front();
+            front = to_assign;
+            auto&& back = queue.back();
+            back = to_assign;
+        }
+
+        queue.pop();
+    }
+}
+
 BENCHMARK(QueuePushing<PairQueueListType<double>>)
     ->RangeMultiplier(RangeMultiplier)->Range(RangeStart, RangeFinish)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK(QueuePushing<PairQueueStackType<double>>)
     ->RangeMultiplier(RangeMultiplier)->Range(RangeStart, RangeFinish)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK(QueueHeavyPushing<PairQueueListType<std::vector<double>>>)
+    ->RangeMultiplier(RangeMultiplier)->Range(HeavyRangeStart, HeavyRangeFinish)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK(QueueHeavyPushing<PairQueueStackType<std::vector<double>>>)
+    ->RangeMultiplier(RangeMultiplier)->Range(HeavyRangeStart, HeavyRangeFinish)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
